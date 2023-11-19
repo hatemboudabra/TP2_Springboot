92fdf50cd49498ba27788ff89cd75cb8d9f40cea
hatemboudabra <132352084+hatemboudabra@users.noreply.github.com>
2023-11-19 23:33:58 +0100

    commit

---
+++ src/main/java/net/javaguides/springboot/springbootbackend/config/ApplicationRequestFilter.java
@@ -0,0 +1,59 @@
+package net.javaguides.springboot.springbootbackend.config;
+
+import net.javaguides.springboot.springbootbackend.Services.auth.ApplicationUserDetailsService;
+import net.javaguides.springboot.springbootbackend.utils.JwtUtil;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ApplicationRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private ApplicationUserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        String userEmail = null;
+        String jwt = null;
+        String idEntreprise = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            userEmail = jwtUtil.extractUsername(jwt);
+            idEntreprise = jwtUtil.extractIdEntreprise(jwt);
+        }
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        MDC.put("idEntreprise", idEntreprise);
+        chain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/net/javaguides/springboot/springbootbackend/config/FlickrConfiguration.java
@@ -0,0 +1,37 @@
+package net.javaguides.springboot.springbootbackend.config;
+
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.RequestContext;
+import com.flickr4java.flickr.auth.Auth;
+import com.flickr4java.flickr.auth.Permission;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class FlickrConfiguration {
+    @Value("${flickr.apiKey}")
+    private String apiKey;
+
+    @Value("${flickr.apiSecret}")
+    private String apiSecret;
+
+    @Value("${flickr.appKey}")
+    private String appKey;
+
+    @Value("${flickr.appSecret}")
+    private String appSecret;
+    @Bean
+    public Flickr getFlickr2() {
+        Flickr flickr = new Flickr(apiKey, apiSecret, new REST());
+        Auth auth = new Auth();
+        auth.setPermission(Permission.READ);
+        auth.setToken(appKey);
+        auth.setTokenSecret(appSecret);
+        RequestContext requestContext = RequestContext.getRequestContext();
+        requestContext.setAuth(auth);
+        flickr.setAuth(auth);
+        return flickr;
+    }
+
+}
